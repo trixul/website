ef50e407b7e85af9fe842cfa17ed195f28fdd96d
Trisha <132001977+trixul@users.noreply.github.com>
2024-10-22 16:30:11 +0000

    Added html and css

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask # Flask = object
+from flask import Flask, render_template # Flask = object
 app = Flask(__name__) # app = object of class                           Flask
 
 @app.route("/") # decorator
 def hello_world():
-  return("Hello Trisha")
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
